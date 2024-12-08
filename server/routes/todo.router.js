8aebe42c0a127b553538ab0ea73f33c38f8b6537
Anthony Clarke <anthonydeandreclarke@gmail.com>
2024-12-07 19:20:30 -0600

    added GET route

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get( '/', ( req, res )=>{
+    console.log( '/todo GET' );
+    // assemble query
+    const queryText = 'SELECT * FROM todos';
+    // run pool.query
+    pool.query( queryText ).then( ( results )=>{
+        // return results.rows
+        res.send( results.rows );
+    }).catch( ( err )=>{
+        // handle any errors
+        console.log( err );
+        res.sendStatus( 400 );
+    })
+})
 // POST
 
 // PUT
--- server/server.js
+++ server/server.js
@@ -3,13 +3,23 @@ const app = express();
 const todoRouter = require('./routes/todo.router.js');
 const PORT = process.env.PORT || 5001;
 
+// adding Data to align to the DB attributes
+const toDoList = [{
+    text: "TEXT",
+    isComplete: false
+  }]
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/todo', todoRouter);
-
+// Adding Get Route
+app.get('/todos', (req, res) => {
+    console.log(`In /todos GET`);
+    res.send(toDoList);
+  });
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
     console.log('Listening on port: ', PORT);
